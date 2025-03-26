cee565d0c0ccc01526aad257603dbf87ad50fb5e
mbali-18 <faithfulservent18@gmail.com>
2025-03-26 16:42:58 +0200

    New project(Routing)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
