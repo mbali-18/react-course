c484283192f405877294696d1f4a06036a49ae9b
mbali-18 <faithfulservent18@gmail.com>
2025-03-27 21:50:43 +0200

    Wip...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Routes, Route } from "react-router";
+
 import AllMeetups from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./Components/layout/MainNavigation";
 
-function App(){
-  return(
+function App() {
+  return (
     <Routes>
-    <Route path="/" element={<AllMeetups />} />
-    <Route path="/new-meetup" element={<NewMeetupPage />} />
-    <Route path="/favorites" element={<FavoritesPage />} />
-  </Routes>
-  )
+      <MainNavigation />
+      <Route path="/" element={<AllMeetups />} />
+      <Route path="/new-meetup" element={<NewMeetupPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/Components/layout/MainNavigation.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router";
+function MainNavigation() {
+  return (
+    <header>
+      <div>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorites</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
